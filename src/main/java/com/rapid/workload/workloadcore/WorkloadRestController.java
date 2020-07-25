fae6a8fd1f96d2f7f7753e5464e6154b73b55c70
shahzad_gh <shehzad.anjum@gmail.com>
2020-07-25 16:04:26 -0400

    adding endpoints

--- src/main/java/com/rapid/workload/workloadcore/WorkloadRestController.java
+++ src/main/java/com/rapid/workload/workloadcore/WorkloadRestController.java
@@ -1,8 +1,13 @@
 package com.rapid.workload.workloadcore;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,9 +18,8 @@ import com.rapid.workload.jpa.SubOrderRepository;
 @RestController
 @RequestMapping(path="/api")
 public class WorkloadRestController {
-	private static final String template = "Hello, %s!";
 	
-private SubOrderRepository subOrderRepository;
+	private SubOrderRepository subOrderRepository;
 	
 	@Autowired
 	org.springframework.jdbc.core.JdbcTemplate JdbcTemplate;
@@ -26,4 +30,25 @@ private SubOrderRepository subOrderRepository;
 		
 	    return subOrderRepository.findAll();
 	}
+	
+	@RequestMapping(value = "/processSubOrder", method = RequestMethod.POST,
+			consumes={MediaType.APPLICATION_XML_VALUE, MediaType.APPLICATION_JSON_VALUE},
+					 produces={MediaType.APPLICATION_XML_VALUE, MediaType.APPLICATION_JSON_VALUE})
+	  public ResponseEntity<?> processSubOrder(@RequestBody Integer subOrderId) throws Exception {
+		
+		//subOrderRepository.save(arg0);
+		
+		return new ResponseEntity<>( HttpStatus.OK);
+	}
+	
+	@RequestMapping(value = "/consolidateOrder", method = RequestMethod.POST,
+			consumes={MediaType.APPLICATION_XML_VALUE, MediaType.APPLICATION_JSON_VALUE},
+					 produces={MediaType.APPLICATION_XML_VALUE, MediaType.APPLICATION_JSON_VALUE})
+	  public ResponseEntity<?> consolidateOrder(@RequestBody Integer orderId) throws Exception {
+		
+		//subOrderRepository.save(arg0);
+		
+		return new ResponseEntity<>( HttpStatus.OK);
+	}
+	
 }
